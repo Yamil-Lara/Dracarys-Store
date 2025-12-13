196cfc36ada9eec002fd8e90df10bee67a2ec90c
Yamil-Lara <143272480+Yamil-Lara@users.noreply.github.com>
2025-12-13 01:26:46 -0400

    Implementación
    Se implementó Página => Corriendo al 100 %

--- DevsUp/urls.py
+++ DevsUp/urls.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
-from django.urls import include,path
+from django.urls import include, path
+from django.conf import settings            # Importar settings
+from django.conf.urls.static import static  # Importar static
 
-from sitioWeb.views import baseView, login_view ,registroView
+from sitioWeb.views import baseView, login_view, registroView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('sitioWeb.urls')),
 ]
+
+# Esto es lo que te falta:
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-este es el readme
+Admin Acces: 
+Nombre => admin
+Contraseña => 123
+
+
+Empezando a CORRER:
+# 1. Crear entorno virtual
+python -m venv venv
+
+# 2. Activar entorno virtual
+# En Windows:
+venv\Scripts\activate
+# En Mac/Linux:
+source venv/bin/activate
+
+# 3. Django: El framework base.
+pip install django django-jazzmin
+
+# 4. Django necesita librería llamada "Pillow"
+pip install Pillow
+
+# 5. Ejecutar el servidor
+python manage.py runserver
+
+# Acceder a la página
+http://127.0.0.1:8000/
